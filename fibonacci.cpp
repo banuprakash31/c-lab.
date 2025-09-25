b416aab22fc1860c074ffb3d7971102b00da2a77
banuprakash31 <banuprakashbalu31@gmail.com>
2025-09-24 23:17:54 -0700

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+
+	int n,first=0,second=1,next,i;
+	
+	printf("enter the number of terms:");
+	scanf("%d",&n);
+	
+	printf("Fibonacci series:");
+	
+	for(i=0;i<n;i++){
+	if(i<=1){
+	next=i;
+	}else{
+	next=first+second;
+	first=next;
+	second = next;
+	}
+	printf("%d",next);
+    }
+	printf("\n");
+    
+    return 0;
+	}
+
+
+
