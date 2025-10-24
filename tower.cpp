e441a019c24e9993a3e9e9b2e0bc358cede80dd9
banuprakash31 <banuprakashbalu31@gmail.com>
2025-10-24 21:19:09 +0530

    Add files via upload

---
+++ tower.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	char ch;
+	int lines,i,j;
+	printf("Enter the character to print:");
+	scanf("%d",&lines);
+	
+	printf("Enter the number of lines:");
+	scanf("%d",&lines);
+	for(i =1; i <= lines;i++);
+	{
+	for(j = 1; j <= i; j++);
+	{
+	printf("%d",ch);
+	}
+	printf("\n");
+	}
+	return 0;
+}
